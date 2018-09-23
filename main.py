db9194ffcfa8d4c8f000c83eed3019d2146031da
zhangzz <zhangzz7998@163.com>
2018-09-23 14:45:47 +0800

    脚本启动和添加迁移命令

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 
@@ -31,6 +33,12 @@ db = SQLAlchemy(app)
 sr = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 创建Session存储对象
 Session(app)
+# 创建管理器
+mgr = Manager(app)
+# 创建迁移器
+Migrate(app, db)
+# 添加迁移命令
+mgr.add_command("mc", MigrateCommand)
 
 
 @app.route('/')
@@ -40,4 +48,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    mgr.run()
