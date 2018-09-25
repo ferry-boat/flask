ab160d077dd7867973e93ae952cc3626815ae0cf
zhangzz <zhangzz7998@163.com>
2018-09-25 15:00:02 +0800

    设置网站小图标

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -10,3 +10,12 @@ from flask import current_app, render_template
 def index():
 
     return render_template("index.html")
+
+
+# 设置网站小图标
+@home_blu.route('/favicon.ico')
+def favicon():
+    # 返回一张图标图片
+    # flask中内置了语法，可以返回静态文件
+    # flask中内置的访问静态文件的路由也会调用该方法
+    return current_app.send_static_file("news/favicon.ico")
