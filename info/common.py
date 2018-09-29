fe7dc0abdbe4cd0607744c16f67bddbda2a34777
zhangzz <zhangzz7998@163.com>
2018-09-29 15:07:27 +0800

    详情页显示登录信息

--- info/common.py
+++ info/common.py
@@ -1,4 +1,33 @@
 # 过滤器的本质就是函数
+import functools
+
+from flask import session, current_app, g
+
+from info.models import User
+
+
 def func_index_convert(index):
     index_dict = {1: "first", 2: "second", 3: "third"}
     return index_dict.get(index, "")
+
+
+# 装饰器来封装登录信息的查询
+def user_login_data(f):  # f = news_detail
+
+    @functools.wraps(f)  # 该装饰器可以让闭包函数使用指定函数的信息
+    def wrapper(*args, **kwargs):
+        # 判断用户是否登录 `session中是否保存了user_id`
+        user_id = session.get("user_id")
+        user = None
+        if user_id:  # 已登录
+            # 根据`user_id`获取`用户信息
+            try:
+                user = User.query.get(user_id)
+            except BaseException as e:
+                current_app.logger.error(e)
+
+        g.user = user
+
+        return f(*args, **kwargs)
+
+    return wrapper
--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,5 +1,6 @@
-from flask import current_app, abort, render_template
+from flask import current_app, abort, render_template, g
 
+from info.common import user_login_data
 from info.constants import CLICK_RANK_MAX_NEWS
 from info.models import News
 from info.modules.news import news_blu
@@ -7,6 +8,7 @@ from info.modules.news import news_blu
 
 # 详情页面
 @news_blu.route('/<int:news_id>')
+@user_login_data  #  news_detail = user_login_data(news_detail)
 def news_detail(news_id):
 
     # 根据新闻id查询该新闻所有的数据
@@ -28,5 +30,7 @@ def news_detail(news_id):
 
     news_list = [news.to_basic_dict() for news in news_list]
 
+    user = g.user.to_dict() if g.user else None
+
     # 将数据传入模板, 进行模板渲染
-    return render_template("detail.html", news=news.to_dict(), news_list=news_list)
+    return render_template("detail.html", news=news.to_dict(), news_list=news_list, user=user)
