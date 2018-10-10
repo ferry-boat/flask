72b4e77b912876fa414f497b9f47ae8ca884f7e3
zhangzz <zhangzz7998@163.com>
2018-10-10 15:04:25 +0800

    后台状态保持

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, current_app, redirect, url_for
+from flask import render_template, request, current_app, redirect, url_for, session
 
 from info.models import User
 from info.modules.admin import admin_blu
@@ -9,6 +9,12 @@ from info.modules.admin import admin_blu
 def login():
 
     if request.method == 'GET': # 显示页面
+        # 取出session中的数据, 如果已登录, 则直接重定向到后台首页
+        user_id = session.get("user_id")
+        is_admin = session.get("is_admin")
+        if user_id and is_admin:
+            return redirect(url_for("admin.index"))
+
         return render_template("admin/login.html")
 
     # POST处理
@@ -32,6 +38,10 @@ def login():
     if not user.check_password(password):
         return render_template("admin/login.html", errmsg="用户名/密码错误")
 
+    # session记录状态信息
+    session["user_id"] = user.id
+    session["is_admin"] = True
+
     # 重定向到后台首页
     return redirect(url_for("admin.index"))
 
@@ -42,3 +52,11 @@ def login():
 def index():
 
     return render_template("admin/index.html")
+
+
+# 后台退出登录
+@admin_blu.route('/logout')
+def logout():
+    session.pop("user_id", None)
+    session.pop("is_admin", None)
+    return redirect(url_for("home.index"))
